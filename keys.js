a86b9ddcdd90a40c1577fbee85b70c9c52cd934e
Jamela314 <jamela314@gmail.com>
2017-07-24 10:07:07 -0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+exports.twitterKeys = {
+  consumer_key: '5FXHM9akrfGK399D2RKZz5deS',
+  consumer_secret: 'ctynjJZrkBmAilcH29vxkdtUNPNsay13cu8PapABeDJ7BDYcH4',
+  access_token_key: '886240147171364866-xj24uXvCDTSRvrmF3lJTXyP1iGy5YKQ',
+  access_token_secret: 'MWeP8VOf5mq0nhE2LkrnHp8FTAAQEb4CsAjsLZ1Au8lo2',
+}
